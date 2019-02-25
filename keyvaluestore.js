6946577766b47e8916f32588d7bf16514e100489
Frank <javier.francisco01.09@gmail.com>
2019-02-24 19:48:23 -0600

    aaa

--- keyvaluestore.js
+++ keyvaluestore.js
@@ -1,56 +1,120 @@
-  var AWS = require('aws-sdk');
-  AWS.config.loadFromPath('./config.json');
+var AWS = require("aws-sdk");
+AWS.config.loadFromPath("./config.json");
 
-  var db = new AWS.DynamoDB();
+var db = new AWS.DynamoDB();
 
-  function keyvaluestore(table) {
-    this.LRU = require("lru-cache");
-    this.cache = this.LRU({ max: 500 });
-    this.tableName = table;
-  };
+function keyvaluestore(table) {
+  this.LRU = require("lru-cache");
+  this.cache = new this.LRU({ max: 500 });
+  this.tableName = table;
+}
 
-  /**
-   * Initialize the tables
-   * 
-   */
-  keyvaluestore.prototype.init = function(whendone) {
-    
-    var tableName = this.tableName;
-    var self = this;
-    
-    
-    whendone(); //Call Callback function.
-  };
+/**
+ * Initialize the tables
+ *
+ */
+keyvaluestore.prototype.init = function(whendone) {
+  var tableName = this.tableName;
+  var self = this;
 
-  /**
-   * Get result(s) by key
-   * 
-   * @param search
-   * 
-   * Callback returns a list of objects with keys "inx" and "value"
-   */
-  
-keyvaluestore.prototype.get = function(search, callback) {
-    var self = this;
-    
-    if (self.cache.get(search))
-          callback(null, self.cache.get(search));
-    else {
-        
-      /*
-       * 
-       * La función QUERY debe generar un arreglo de objetos JSON son cada
-       * una de los resultados obtenidos. (inx, value, key).
-       * Al final este arreglo debe ser insertado al cache. Y llamar a callback
-       * 
-       * Ejemplo:
-       *    var items = [];
-       *    items.push({"inx": data.Items[0].inx.N, "value": data.Items[0].value.S, "key": data.Items[0].key});
-       *    self.cache.set(search, items)
-       *    callback(err, items);
-       */
+  db.describeTable({ TableName: tableName }, function(err, data) {
+    if (err) {
+      var params = {
+        AttributeDefinitions: [
+          {
+            AttributeName: "Keyword",
+            AttributeType: "S"
+          },
+          {
+            AttributeName: "inx",
+            AttributeType: "N"
+          }
+        ],
+        KeySchema: [
+          {
+            AttributeName: "Keyword",
+            KeyType: "HASH"
+          },
+          {
+            AttributeName: "inx",
+            KeyType: "RANGE"
+          }
+        ],
+        ProvisionedThroughput: {
+          ReadCapacityUnits: 1,
+          WriteCapacityUnits: 1
+        },
+        TableName: tableName
+      };
+      db.createTable(params, function(err, data) {
+        if (err) console.log(err, err.stack);
+        // an error occurred
+        else console.log(data); // successful response
+      });
+    } else {
+      console.log(data);
     }
-  };
+  });
 
+  whendone(); //Call Callback function.
+};
 
-  module.exports = keyvaluestore;
+/**
+ * Get result(s) by key
+ *
+ * @param search
+ *
+ * Callback returns a list of objects with keys "inx" and "value"
+ */
+
+keyvaluestore.prototype.get = function(search, callback) {
+  var self = this;
+
+  if (self.cache.get(search)) callback(null, self.cache.get(search));
+  else {
+    /*
+     *
+     * La función QUERY debe generar un arreglo de objetos JSON son cada
+     * una de los resultados obtenidos. (inx, value, key).
+     * Al final este arreglo debe ser insertado al cache. Y llamar a callback
+     *
+     * Ejemplo:
+     *    var items = [];
+     *    items.push({"inx": data.Items[0].inx.N, "value": data.Items[0].value.S, "key": data.Items[0].key});
+     *    self.cache.set(search, items)
+     *    callback(err, items);
+     */
+    var searchValues = search.split(" ");
+    var items = [];
+    searchValues.forEach(function(value) {
+      var params = {
+        ExpressionAttributeValues: {
+          ":v1": {
+            S: value
+          }
+        },
+        KeyConditionExpression: "Keyword = :v1",
+        TableName: self.tableName
+      };
+
+      db.query(params, function(err, data) {
+        if (err) {
+          console.log(err);
+        } else {
+          data.Items.forEach(function(item) {
+            items.push({
+              inx: item.inx.N,
+              value: item.Value.S,
+              key: item.Keyword.S
+            });
+          });
+          self.cache.set(search, items);
+          console.log(items);
+          callback(err, items);
+        }
+      });
+    });
+  }
+};
+
+module.exports = keyvaluestore;
--- public/js/app.js
+++ public/js/app.js
@@ -1,46 +1,99 @@
+var index = 0;
+var data = [];
 $(document).ready(function() {
   $("#version").html("v0.14");
-  
-  $("#searchbutton").click( function (e) {
+
+  $("#searchbutton").click(function(e) {
     displayModal();
   });
-  
-  $("#searchfield").keydown( function (e) {
-    if(e.keyCode == 13) {
-      displayModal();
-    }	
-  });
-  
-  function displayModal() {
-    $(  "#myModal").modal('show');
 
+  $("#searchfield").keydown(function(e) {
+    if (e.keyCode == 13) {
+      displayModal();
+    }
+  });
+
+  function displayModal() {
+    $("#myModal").modal("show");
     $("#status").html("Searching...");
-    $("#dialogtitle").html("Search for: "+$("#searchfield").val());
+    $("#dialogtitle").html("Search for: " + $("#searchfield").val());
     $("#previous").hide();
     $("#next").hide();
-    $.getJSON('/search/' + $("#searchfield").val() , function(data) {
+    $.getJSON("/search/" + $("#searchfield").val(), function(data) {
       renderQueryResults(data);
     });
   }
-  
-  $("#next").click( function(e) {
 
+  $("#next").click(function(e) {
+    if (index + 1 > data.length / 4) {
+      console.log(index);
+    } else {
+      index++;
+      $("#pic0").attr("src", data.results[index * 4]);
+      $("#pic1").attr("src", data.results[1 + index * 4]);
+      $("#pic2").attr("src", data.results[2 + index * 4]);
+      $("#pic3").attr("src", data.results[3 + index * 4]);
+    }
   });
-  
-  $("#previous").click( function(e) {
-  
+
+  $("#previous").click(function(e) {
+    if (index - 1 < 0) {
+      console.log(index);
+    } else {
+      index--;
+      $("#pic0").attr("src", data.results[index * 4]);
+      $("#pic1").attr("src", data.results[1 + index * 4]);
+      $("#pic2").attr("src", data.results[2 + index * 4]);
+      $("#pic3").attr("src", data.results[3 + index * 4]);
+    }
   });
 
   function renderQueryResults(data) {
-    
-    if (data.error != undefined) {
-      $("#status").html("Error: "+data.error);
-    } else {
-      $("#status").html(""+data.num_results+" result(s)");
+    $("#pic0").attr("src", "");
+    $("#pic1").attr("src", "");
+    $("#pic2").attr("src", "");
+    $("#pic3").attr("src", "");
+    $("#pic0").hide();
+    $("#pic1").hide();
+    $("#pic2").hide();
+    $("#pic3").hide();
+    $("#previous").hide();
+    $("#next").hide();
 
-      $("#next").show();
-      $("#previous").show();
-      
-     }
-   }
+    if (data.error != undefined) {
+      $("#status").html("Error: " + data.error);
+      $("#pic0").attr("src", "");
+      $("#pic1").attr("src", "");
+      $("#pic2").attr("src", "");
+      $("#pic3").attr("src", "");
+    } else {
+      this.data = data;
+      $("#status").html("" + data.num_results + " result(s)");
+
+      if (data.results.length > 4) {
+        $("#next").show();
+        $("#previous").show();
+      }
+
+      if (data.results.length >= 1) {
+        $("#pic0").show();
+        $("#pic0").attr("src", data.results[0]);
+      }
+
+      if (data.results.length >= 2) {
+        $("#pic1").show();
+        $("#pic1").attr("src", data.results[1]);
+      }
+
+      if (data.results.length >= 3) {
+        $("#pic2").show();
+        $("#pic2").attr("src", data.results[2]);
+      }
+
+      if (data.results.length >= 4) {
+        $("#pic3").show();
+        $("#pic3").attr("src", data.results[3]);
+      }
+    }
+  }
 });
